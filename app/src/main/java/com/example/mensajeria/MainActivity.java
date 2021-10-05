3ce72bfc800551aafc54e9384944cb86d688baa1
JoseMario28 <josemarioac@hotmail.es>
2021-10-05 17:26:09 +0200

    Version FINAL

--- app/src/main/java/com/example/mensajeria/MainActivity.java
+++ app/src/main/java/com/example/mensajeria/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mensajeria;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,14 +21,30 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edt_enviado1 = null;
     private TextView txt_recibido1 = null;
+    ActivityResultLauncher<Intent> someActivityResultLauncher = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edt_enviado1 = (EditText) findViewById(R.id.edt_enviado1);
         txt_recibido1 = (TextView) findViewById(R.id.txt_recibido1);
-    }
 
+
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            String texto = data.getStringExtra(pantalla2Activity.EXTRA_RESPUESTA2);
+                            txt_recibido1.setText(texto);
+                        }
+                    }
+                });
+    }
+/*
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -35,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
+*/
     public void enviar1(View view) {
         String texto = String.valueOf(edt_enviado1.getText());
         Intent intent = new Intent(this, pantalla2Activity.class);
@@ -43,5 +64,7 @@ public class MainActivity extends AppCompatActivity {
         //startActivity(intent);// enviar las cosas sin esperar respuestas
         //Iniciar un activity y esperar una respuesta
         startActivityForResult(intent, PETICION1);
+
+        someActivityResultLauncher.launch(intent);
     }
 }
